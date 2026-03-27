78a9ad60c39176a0d1b4a9646d53e72ec3284850
Playerking24 <rodriguezcruznickcarlos@gmail.com>
2026-03-27 17:47:36 +0100

    Examen resuelto

--- examen.js
+++ examen.js
@@ -1,5 +1,15 @@
 // declarame variable de tipo numero, 
 // dos variables, luego crea una tercera y 
-// haz suma, resta, mukltipliocacion y division 
+// haz suma, resta, multiplicacion y division 
 // con las dos primeras variables, 
 // luego muestra el resultado en consola.
+
+var One = 1
+var six = 6
+var Two = 2
+
+
+One + six
+six * One
+One - One
+six/six
