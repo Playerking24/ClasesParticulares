09659babfa10c730e9f6579ff93f7d163914c72b
Nicoguzok <near.ng7@gmail.com>
2026-03-27 17:43:06 +0100

    examen

---
+++ examen.js
@@ -0,0 +1,5 @@
+// declarame variable de tipo numero, 
+// dos variables, luego crea una tercera y 
+// haz suma, resta, mukltipliocacion y division 
+// con las dos primeras variables, 
+// luego muestra el resultado en consola.
